fcc1b83f3b406892796a0026c5e71674e068c5b0
Sandamini Indrachapa <130564612+SandaminiI@users.noreply.github.com>
2026-01-02 11:51:41 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 import express from "express";
-//import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
@@ -8,27 +7,25 @@ import cors from "cors";
 import { connectDB } from "./config/db.js";
 import { notFound, errorHandler } from "./middlewares/error.js";
 import authRoutes from "./routes/authRoutes.js";
+import mediaRoutes from "./routes/mediaRoutes.js";
+import contactRoutes from "./routes/contactRoutes.js";
+import adminContactRoutes from "./routes/adminContactRoutes.js";
+import adminUserRoutes from "./routes/adminUserRoutes.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 await connectDB();
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-
-
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// app.use(
-//   cors({
-//     origin: process.env.FRONTEND_ORIGIN,
-//     credentials: true
-//   })
-// );
-
 app.use(morgan("dev"));
-
 app.use(
   rateLimit({
     windowMs: 15 * 60 * 1000,
@@ -36,8 +33,19 @@ app.use(
   })
 );
 
+// serve images
+app.use("/uploads", (req, res, next) => {
+  res.setHeader("Cross-Origin-Resource-Policy", "cross-origin");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  next();
+}, express.static(path.join(__dirname, "uploads")));
+
 //routes
 app.use("/api/auth", authRoutes);
+app.use("/api/media", mediaRoutes);
+app.use("/api/contact", contactRoutes);
+app.use("/api/admin/contact", adminContactRoutes);
+app.use("/api/admin/users", adminUserRoutes);
 
 app.get("/health", (req, res) => {
   res.json({ ok: true, message: "Backend running" });
